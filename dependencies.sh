04fc7d3c5511a7f11d4c4d5484f03a1a83eea409
Manh Ha Hoang <manh.ha.hoang@ipa.fraunhofer.de>
2019-07-31 11:11:04 +0200

    update ReadMe

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,2 @@
 sudo apt-get update
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
-sudo apt-get update
-sudo apt-get install -y libglm-dev libglew-dev mesa-utils freeglut3 freeglut3-dev
+sudo apt-get install -y libglm-dev libglew-dev mesa-utils freeglut3 freeglut3-dev libglfw3-dev libglfw3
