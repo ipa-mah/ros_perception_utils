d4455f1d080769083447f8756df72b76c18a7d51
Manh Ha Hoang <manh.ha.hoang@ipa.fraunhofer.de>
2019-07-31 10:10:56 +0200

    update

--- dependencies.sh
+++ dependencies.sh
@@ -1,4 +1,6 @@
 sudo apt-get update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6B05F25D762E3157
+sudo apt-get update
 sudo apt-get install -y libglm-dev libglew-dev mesa-utils freeglut3 freeglut3-dev 
 sudo apt-get update
 sudo apt-get install -y libglfw3-dev libglfw3
