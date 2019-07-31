47f1434646667b0af36f79e90119fc120c080713
Manh Ha Hoang <manh.ha.hoang@ipa.fraunhofer.de>
2019-07-31 10:08:07 +0200

    update

--- dependencies.sh
+++ dependencies.sh
@@ -1,2 +1,4 @@
 sudo apt-get update
-sudo apt-get install -y libglm-dev libglew-dev mesa-utils freeglut3 freeglut3-dev libglfw3-dev libglfw3
+sudo apt-get install -y libglm-dev libglew-dev mesa-utils freeglut3 freeglut3-dev 
+sudo apt-get update
+sudo apt-get install -y libglfw3-dev libglfw3
